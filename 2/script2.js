bc3c005e7183e0d0cda7a32f0b1a93e9a0ee38fe
Nawell <nawell.sadout-derraz@dauphine.eu>
2019-06-18 16:51:49 +0200

    Defis

--- 2/script2.js
+++ 2/script2.js
@@ -1,14 +1,18 @@
 $( document ).ready(function() {
   alert('Hello')
-   		function getUsername() {
+  	function welcome() {
+   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+};
+})
+function getUsername() {
     //affiche une boite de dialogue 
     var  user = prompt("Veuillez rentrez votre nom:", "");
     // la fonction se termine en retournant le nom récupéré par la boite de dialogue
     return user
-     	function welcome() {
+}
+
+ function welcome() {
     var name= getUsername();
     $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
 };
-})
-}
-};;
