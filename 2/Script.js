ca8b8fe839cb552b72f3bdbf7e6df027f8415a54
Nawell <nawell.sadout-derraz@dauphine.eu>
2019-06-29 12:12:05 +0200

    Defis Bienvenue + Nom + Appel
    Je n'arrive pas à faire apparaitre mon nom dans l'encart bleu

--- 2/Script.js
+++ 2/Script.js
@@ -1,14 +1,54 @@
-$( document ).ready(function() {
-  alert('Hello')
-   		function getUsername() {
+
+function getUsername() {
     //affiche une boite de dialogue 
     var  user = prompt("Veuillez rentrez votre nom:", "");
     // la fonction se termine en retournant le nom récupéré par la boite de dialogue
     return user
-     	function welcome() {
+}
+
+ function welcome() {
     var name= getUsername();
     $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
 };
-})
+
+welcome();
+
+function welcome() {
+   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+};
+welcome();
+
+function loadChallenges() {
+  $.ajax(
+        {url : 'https://s3.eu-central-1.amazonaws.com/spintransfer/challenge.json',
+        type: 'GET',
+        dataType: 'json'} 
+  )
+  .done(function(data) {
+    showChallenge(data);
+  })
+  .fail(function() {
+    alert( "error" );
+  });
 }
-};;
+function showChallenge(liste) {
+
+  var data="";
+
+  for ( var i=0; i < liste.length; i++) {
+    data+='<div class="defi">';
+
+    var challenge=liste[i];
+
+    data+='<h2>'+challenge.nom+'</h2>';
+    data+='<p>'+challenge.description+'</p>';
+    data+='<iframe width="364" height="204" src="'+challenge.youtube+'" frameborder="0" allowfullscreen=""></iframe>'
+    data+="</div>";
+  }
+  $("#defis").html(data);
+
+}
+
+
+loadChallenges();
